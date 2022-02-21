0f179249429c279670dddc334d585e3c77d43c37
MinhTuanCNH <99940999+MinhTuanCNH@users.noreply.github.com>
2022-02-21 13:58:25 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define fi first
+#define se second
+#define sz(s) int(s.size())
+
+#define vi vector<int>
+#define vll vector<long long>
+
+const long long mod = 1e9 + 7;
+const long long inf = 1e18 - 1;
+const int MaxN1 = 1e5 + 5;
+const int MaxN2 = 1e3 + 5;
+
+using namespace std;
+
+signed main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  return 0;
+}
+
+// tuntun
